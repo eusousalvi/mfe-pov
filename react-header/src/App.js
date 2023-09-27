c80c67c91517303095ba435896ca8a5fd2dce0ed
Gabriel Santana Da Silva <gsantana@192.168.1.9>
2023-09-27 09:31:50 -0300

    chore: upload work pov

--- react-header/src/App.js
+++ react-header/src/App.js
@@ -1,31 +1,39 @@
-import React from 'react'
-import { Link, BrowserRouter } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import { Link, BrowserRouter } from "react-router-dom";
 
-const App = ({ name }) => (
-  <BrowserRouter>
-    <header>
-      <h1>{name}</h1>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/react-single">Single</Link>
-          </li>
-          <li>
-            <Link to="/react-lazy">Lazy</Link>
-          </li>
-          <li>
-            <Link to="/react-multiples">Multiples</Link>
-          </li>
-          <li>
-            <Link to="/react-route">Route</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </BrowserRouter>
-)
+const App = ({ name }) => {
+  const [apps, setApps] = useState([]);
 
-export default App
+  useEffect(() => {
+    fetch("http://localhost:3000/applications")
+      .then((resp) => resp.json())
+      .then((data) => {
+        setApps(data);
+      });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      <header>
+        <h1>{name}</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            {apps &&
+              apps
+                .filter((app) => app?.menu)
+                .map((app) => (
+                  <li>
+                    <Link to={app?.route}>{app?.menu}</Link>
+                  </li>
+                ))}
+          </ul>
+        </nav>
+      </header>
+    </BrowserRouter>
+  );
+};
+
+export default App;
--- single-spa/src/mc-root-config.js
+++ single-spa/src/mc-root-config.js
@@ -1,20 +1,22 @@
 import { registerApplication, start } from "single-spa";
 
-fetch('https://run.mocky.io/v3/b2ed2729-fd28-4a2b-bc5b-987e3e69cc3d')
-  .then(resp => resp.json())
-  .then(data => {
-    data.applications.forEach(app => {
-      registerApplication({
-        name: app.name,
-        app: () => System.import(app.package),
-        activeWhen: app.exact
-          ? (location) => location.pathname === app.activeWhen
-          : [app.activeWhen]
+fetch("http://localhost:3000/applications")
+  .then((resp) => resp.json())
+  .then((data) => {
+    data
+      .filter((app) => app?.show)
+      .forEach((app) => {
+        registerApplication({
+          name: app.name,
+          app: () => System.import(app.package),
+          activeWhen: app.exact
+            ? (location) => location.pathname === app.route
+            : [app.route],
+        });
       });
-    })
   })
   .finally(() => {
     start({
       urlRerouteOnly: true,
-    })
-  })
+    });
+  });
